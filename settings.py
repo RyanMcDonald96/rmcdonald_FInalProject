c74b27013394f659368d017eaaa953fc67f74a51
RyanMcDonald96 <rhinomac23@gmail.com>
2023-05-16 11:51:54 -0700

    trying to break up code i have more lines

---
+++ settings.py
@@ -0,0 +1,21 @@
+# By Ryan McDonald
+# Player settings
+player_fric = -0.2
+player_grav = 3
+gravOn = True
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARKBLUE = (26, 17, 112)
+PINK = (232, 151, 152)
+LIGHTGRAY = (212, 212, 212)
+GRAY = (128, 128, 128)
+DARKGRAY = (77, 77, 77)
+PURPLE = (108, 0, 171)
+LIME = (151, 255, 107)
+LIGHTORANGE = (255, 215, 163)
+ORANGE = (237, 134, 0)
